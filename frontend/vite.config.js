4a5b20f9d4b71326ee3a07a5400cd2bc4abfa4a8
Thilina Madhushanka <thilinamadhushanka641@gmail.com>
2025-10-02 22:36:57 +0530

    new

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,6 @@ import tsconfigPaths from "vite-tsconfig-paths"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: "/mern_project",
   resolve: {
     alias: [
       { find: '@frontend', replacement: '/src' },
